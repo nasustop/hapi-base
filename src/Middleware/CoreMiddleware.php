b87a733f19ed086f175401a2d30fb22e44d8e9aa
eaglexpf <1121252990@qq.com>
2022-12-03 16:34:10 +0800

    core middleware add route alias

--- src/Middleware/CoreMiddleware.php
+++ src/Middleware/CoreMiddleware.php
@@ -17,9 +17,25 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class CoreMiddleware extends \Hyperf\HttpServer\CoreMiddleware
 {
+    public function dispatch(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $request = parent::dispatch($request);
+        return $this->addRouteAlias($request);
+    }
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $response = parent::process($request, $handler);
         return $response->withHeader('Server', env('APP_NAME', 'hapi'));
+    }
+
+    protected function addRouteAlias(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $attributes = $request->getAttributes();
+        $attributes = @json_decode(json_encode($attributes), true);
+        $alias = $attributes ? ($attributes['Hyperf\HttpServer\Router\Dispatched']['handler']['options']['alias'] ?? '') : '';
+        $name = $attributes ? ($attributes['Hyperf\HttpServer\Router\Dispatched']['handler']['options']['name'] ?? '') : '';
+        // 获取路由文件中设置的别名和名称
+        return $request->withAttribute('route.alias', $alias)->withAttribute('route.name', $name);
     }
 }
