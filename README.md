5cf641da27849b4e94e15179ad5e4e1e0412b0e5
eaglexpf <1121252990@qq.com>
2023-02-20 10:25:33 +0800

    预备重构

--- README.md
+++ README.md
@@ -14,10 +14,3 @@ class Repository extends \Nasustop\HapiBase\Repository\Repository
     // TODO: 单独建立一个基础类的好处是，如果组件中的方法有不符合自己业务的，可选择重写该方法
 }
 ```
-
-## Queue
-使用以下命令生成queue配置文件
-```shell
-php bin/hyperf.php vendor:publish nasustop/hapi-base
-```
-
--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -11,69 +11,20 @@ declare(strict_types=1);
  */
 namespace Nasustop\HapiBase;
 
-use Hyperf\HttpServer\CoreMiddleware;
-use Nasustop\HapiBase\Auth\AuthManagerFactory;
-use Nasustop\HapiBase\Command\GenCodeCommand;
-use Nasustop\HapiBase\HttpServer\Request;
-use Nasustop\HapiBase\HttpServer\RequestInterface;
-use Nasustop\HapiBase\HttpServer\Response;
-use Nasustop\HapiBase\HttpServer\ResponseInterface;
-use Nasustop\HapiBase\Memcached\Memcached;
-use Nasustop\HapiBase\Middleware\CoreMiddleware as HapiCoreMiddleware;
-use Nasustop\HapiBase\Queue\Amqp\Consumer;
-use Nasustop\HapiBase\Queue\Amqp\ConsumerFactory;
-use Nasustop\HapiBase\Queue\Amqp\Producer;
-use Nasustop\HapiBase\Queue\Command\ConsumerCommand;
-use Nasustop\HapiBase\Queue\Command\ProducerCommand;
-use Nasustop\HapiBase\Queue\Listener\QueueHandleListener;
-
 class ConfigProvider
 {
     public function __invoke(): array
     {
         return [
             'dependencies' => [
-                RequestInterface::class => Request::class,
-                ResponseInterface::class => Response::class,
-                Consumer::class => ConsumerFactory::class,
-                Producer::class => Producer::class,
-                CoreMiddleware::class => HapiCoreMiddleware::class,
-                AuthManagerFactory::class => AuthManagerFactory::class,
-                \Memcached::class => Memcached::class,
             ],
             'commands' => [
-                ConsumerCommand::class,
-                ProducerCommand::class,
-                GenCodeCommand::class,
-            ],
-            'listeners' => [
-                QueueHandleListener::class,
             ],
             'annotations' => [
                 'scan' => [
                     'paths' => [
                         __DIR__,
                     ],
-                ],
-            ],
-            'publish' => [
-                [
-                    'id' => 'auth',
-                    'description' => 'The config for auth.',
-                    'source' => __DIR__ . '/../publish/auth.php',
-                    'destination' => BASE_PATH . '/config/autoload/auth.php',
-                ],
-                [
-                    'id' => 'queue',
-                    'description' => 'The config for queue.',
-                    'source' => __DIR__ . '/../publish/queue.php',
-                    'destination' => BASE_PATH . '/config/autoload/queue.php',
-                ],
-                [
-                    'id' => 'memcached',
-                    'description' => 'The config for memcached.',
-                    'source' => __DIR__ . '/../publish/memcached.php',
-                    'destination' => BASE_PATH . '/config/autoload/memcached.php',
                 ],
             ],
         ];
